6e4cdf5222dbf8cfe94a3c6847d83f2ed3b5fca5
otabek-a <otabekabdurasulov245@gmail.com>
2025-03-13 23:59:52 +0500

    they are done

--- db.py
+++ db.py
@@ -7,40 +7,62 @@ class Smartphone:
 
     def sql_get_all_smartphones(self):
 
-        return
+        self.cursor.execute('select * from smartphone')
+        return self.cursor.fetchall()
+        
     
     def sql_get_product_by_id(self, id):
-
-        return
+        self.cursor.execute('select * from smartphone where id=?',(id,) )
+        return self.cursor.fetchall()
+        
     
     def sql_get_smartphone_by_name(self, name):
 
-        return
+        self.cursor.execute('select * from smartphone where name=?',(name,) )
+        return self.cursor.fetchall()
     
     def sql_get_smartphone_all_names(self):
-
-        return
+        self.cursor.execute('select name from smartphone ' )
+        return self.cursor.fetchall()
     
     def sql_get_smartphone_by_color(self, color):
 
-        return
+        self.cursor.execute('select * from smartphone where color=?',(color,) )
+        return self.cursor.fetchall()
     
     def sql_get_smartphone_by_ram(self, ram):
 
-        return
+        self.cursor.execute('select * from smartphone where ram=?',(ram,) )
+        data= self.cursor.fetchall()
+        return data
     
     def sql_get_smartphone_by_memory(self, memory):
 
-        return
+        self.cursor.execute('select * from smartphone where memory=?',(memory,) )
+        return  self.cursor.fetchall()
+        
     
     def sql_get_smartphone_by_price(self, price):
-
-        return
+        self.cursor.execute('select * from smartphone where price=?',(price,) )
+        return self.cursor.fetchall()
+        
     
     def sql_add_smartphone(self, phone):
-
-        return
+        name=phone.get('name','iphone')
+        color=phone.get('color','white')
+        price=phone.get('price',100)
+        ram=phone.get('ram','4gb')
+        memory=phone.get('memory','128')
+        
+        self.cursor.execute('Insert into smartphone ( name, color, ram, memory, price) VALUES ( ?, ?, ?, ?, ?)',(name, color, ram, memory, price))
+        self.connect.commit()
+        return {'new phone': phone}
+        
     
     def sql_delete_smartphone(self, id):
 
-        return 
+        self.cursor.execute('DELETE FROM smartphone WHERE id = ?',(id,))
+        self.connect.commit()
+        return {'deleted_id':id}
+son=Smartphone('smartphone_store.db')
+# print(son.sql_delete_smartphone(1))
--- flask_app.py
+++ flask_app.py
@@ -21,49 +21,61 @@ def get_product_by_id(id):
 @app.route('/smartphones/name/<name>', methods=['GET'])
 def get_smartphone_by_name(name):
     """Returns a product by name"""
-    return 
+    son=Smartphone('smartphone_store.db')
+    return son.sql_get_smartphone_by_name(name)
+    
 
 # view all smartphones names
 @app.route('/smartphones/names', methods=['GET'])
 def get_smartphone_all_names():
     """Returns all smartphones names"""
-    return
+    son=Smartphone('smartphone_store.db')
+    return son.sql_get_smartphone_all_names()
 
 # view smartphone by color
 @app.route('/smartphones/color/<color>', methods=['GET'])
 def get_smartphone_by_color(color):
     """Returns a smartphones by color"""
-    return 
+    son=Smartphone('smartphone_store.db')
+    return son.sql_get_smartphone_by_color(color)
 
 # view smartphone by ram
 @app.route('/smartphones/ram/<ram>', methods=['GET'])
 def get_smartphone_by_ram(ram):
     """Returns a smartphones by ram"""
-    return 
+    son=Smartphone('smartphone_store.db')
+    return son.sql_get_smartphone_by_ram(ram)
 
 # view smartphone by memory
 @app.route('/smartphones/memory/<memory>', methods=['GET'])
 def get_smartphone_by_memory(memory):
     """Returns a smartphones by memory"""
-    return 
+    son=Smartphone('smartphone_store.db')
+    return son.sql_get_smartphone_by_memory(memory)
+
 
 # view smartphone by price
 @app.route('/smartphones/price/<price>', methods=['GET'])
 def get_smartphone_by_price(price):
     """Returns a smartphones if database in price bigger from price"""
-    return 
+    son=Smartphone('smartphone_store.db')
+    return son.sql_get_smartphone_by_price(float(price))
 
 # view add smartphone
 @app.route('/smartphone/add', methods=['POST'])
 def add_smartphone():
     """Adds a product to the database"""
-    return
+    data=request.get_json()
+    son=Smartphone('smartphone_store.db')
+    return son.sql_add_smartphone(data)
 
 # view delete smartphone
 @app.route('/smartphone/delete/<int:id>', methods=['DELETE'])
 def delete_smartphone(id):
     """Deletes a smartphone from the database"""
-    return 
+    son=Smartphone('smartphone_store.db')
+    return son.sql_delete_smartphone((id))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
